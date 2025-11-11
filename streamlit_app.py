d3af49274fc1b0d163eb6afd70cbc7cfaf33b99a
jwg-86 <h098s02823@chunchon.gwe.hs.kr>
2025-11-11 14:27:45 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -72,7 +72,7 @@ CONTENT_BY_LABEL: dict[str, dict[str, list[str]]] = {
      labels[0]: {
        "texts": ["키오프 나띠는 예쁘다"],
        "images": ["https://i.namu.wiki/i/CuOr1J7xJMwXjDWCKY5XXeEDJi3OE-4uDIN-rCYRBdf0oMSp06AhT7ETlVGTu59nQvpU9nni4P1HjuXttJlfQw.webp"],
-       "videos": ["https://www.youtube.com/shorts/HHuyym_v3fw"]
+       "videos": ["https://www.youtube.com/shorts/1Op4GNQ9i5g"]
      },
 }
 
