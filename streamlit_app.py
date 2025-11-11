35a42135145dd5b7eb7bdea89b72f45344677760
jwg-86 <h098s02823@chunchon.gwe.hs.kr>
2025-11-11 14:22:22 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -72,7 +72,7 @@ CONTENT_BY_LABEL: dict[str, dict[str, list[str]]] = {
      labels[0]: {
        "texts": ["키오프 나띠는 예쁘다"],
     #   "images": ["https://.../jjampong1.jpg", "https://.../jjampong2.jpg"],
-    #   "videos": ["https://youtu.be/XXXXXXXXXXX"]
+       "videos": ["https://www.youtube.com/shorts/HHuyym_v3fw"]
      },
 }
 
